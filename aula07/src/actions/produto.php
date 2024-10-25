f909b71ddfa82e261fcb0293ff6f66f9cac947ee
rodrigopnonato <rodrigopnonato@gmail.com>
2024-10-25 00:00:44 -0300

    initial commit, pagina de inserir, listar e editar produtos, bem como excluir
    - Implementada a classe Porta com métodos para abrir, fechar e verificar estado.
    
    - Implementada a classe Imovel para gerenciar portas, incluindo métodos para adicionar e contar portas abertas.
    
    - Implementada a classe Casa que herda de Imovel, iniciando com três portas padrão.
    
    - Implementada a classe Edificio que herda de Imovel, com métodos para adicionar andares e gerenciar portas.

---
+++ aula07/src/actions/produto.php
@@ -0,0 +1,43 @@
+<?php
+    require $_SERVER['DOCUMENT_ROOT'] .'/inicial/PRW38105-PROGRAMACAO-WEB/aula07/conexao.php';    
+    require $_SERVER['DOCUMENT_ROOT'] .'/inicial/PRW38105-PROGRAMACAO-WEB/aula07/src/database/produto.php';
+   
+    function consultarProdutos(){
+        global $conexao;
+        return consultarProdutosBD($conexao);
+    }
+
+    function consultarProdutoById($id_produto){
+        global $conexao;
+        return consultarProdutoByIdBD($conexao, $id_produto);
+    }
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $id_produto = $_POST['id_produto'];
+        $nome = $_POST['nome'];
+        $descricao = $_POST['descricao'];
+        $quantidade = $_POST['quantidade'];
+        $preco = $_POST['preco'];
+
+        if(isset($_POST['inserir'])){
+
+            if(cadastrarProdutoBD($conexao, $nome, $descricao, $quantidade, $preco)){
+              header('location:../pages/produto/listarProduto.php');  
+            }
+
+        } else if(isset($_POST['editar'])){
+
+            if(editarProdutoBD($conexao, $id_produto, $nome, $descricao, $quantidade, $preco)){
+              header('location:../pages/produto/listarProduto.php');  
+            }
+
+        } else if(isset($_POST['excluir'])){
+            echo 'TESTE';
+            if(excluirProdutoBD($conexao, $id_produto)){
+                echo $id_produto;
+                header('location:../pages/produto/listarProduto.php');  
+            }
+        }
+    }
+?>
